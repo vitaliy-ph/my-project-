4ec65bd9e91d85beb5ee37cfd5aff99e9951b649
vitaliy-ph <vitaliy255555@gmail.com>
2020-10-14 18:48:31 +0300

    home work 3 (loops)

--- samples/html.php
+++ samples/html.php
@@ -10,22 +10,22 @@ if (1 > 0) {
 
 <table >
     <tr>
-        <td>Task id  3323</td>
+        <td>Task id :  3323</td>
     </tr>
     <tr>
-        <td>task title  create a project in PHP</td>
+        <td>task title : create a project in PHP</td>
     </tr>
     <tr>
-        <td>task description  create a project using arrays of loops and functions</td>
+        <td>task description:  create a project using arrays of loops and functions</td>
     </tr>
     <tr>
-        <td>task owner Homer Simpson</td>
+        <td>task owner: Homer Simpson</td>
     </tr>
     <tr>
-        <td>task deadline 18.10.2020, 18:20</td>
+        <td>task deadline: 18.10.2020, 18:20</td>
     </tr>
     <tr>
-        <td>task status done</td>
+        <td>task status: done</td>
     </tr>
 
 </table>
