34b3e56a71ae367c18f4590ec78ea05f7550bfd7
vitaliy-ph <vitaliy255555@gmail.com>
2020-10-15 17:07:44 +0300

    home work 3 (loops and html table)

--- samples/html.php
+++ samples/html.php
@@ -1,101 +1,40 @@
 <?php
 
+
 if (1 > 0) {
     echo '<h1>Task manager</h1>';
 }
 
+$info  = [
+
+    'task id' => 3323,
+    'task title' => 'create a project in PHP',
+    'task description' => 'create a project using arrays of loops and functions',
+    'task owner' => 'Homer Simpson',
+    'task deadline' => '18.10.2020, 18:20',
+    'task status' => 'done',
+];
 ?>
+<table class="Task manager" style="width:50%"
 
-<!DOCTYPE html>
-<html>
-<head>
-    <style>
-        table, th, td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-        th, td {
-            padding: 10px;
-        }
-    </style>
-</head>
-</body>
-
-
-
-<table style="width:50%">
-
-    <tr>
-        <td>Task id</td>
-        <td>3323</td>
-    </tr>
-    <tr>
-        <td>task title</td>
-        <td>create a project in PHP</td>
-    </tr>
-    <tr>
-        <td>task description</td>
-        <td>create a project using arrays of loops and functions</td>
-    </tr>
-    <tr>
-        <td>task owner</td>
-        <td>Homer Simpson</td>
-    <tr>
-        <td>task deadline</td>
-        <td>18.10.2020, 18:20</td>
-
-    </tr>
-    <tr>
-        <td>task status</td>
-        <td>done</td>
-
-    </tr>
-    </tr>
+    <?php
+    foreach($info as $key => $value) {
+        echo '<tr>';
+        echo '<td>' . $key . '</td>';
+        echo '<td>' . ($value) . '</td>';
+        echo '<tr>';
+    }
+    ?>
 </table>
 
-</body>
-</html>
 
 
-
-<!--
-<table class="Task manager" border-collapse="collapse"  style="width: 50%" border="1px" cellpadding="1px" cellspacing="1" margin= "100px"; </table>
-
-</tr><tr><td class="cell c0 lastcol">Task id</td>
-    <td class="cell c0 lastcol">3323</td>
-</tr><tr><td class="cell c0 lastcol">Task title</td>
-    <td class="cell c0 lastcol">create a project in PHP</td>
-</tr><tr><td class="cell c0 lastcol">Task description</td>
-    <td class="cell c0 lastcol">create a project using arrays of loops and functions</td>
-</tr><tr><td class="cell c0 lastcol">Task owner</td>
-    <td class="cell c0 lastcol">Homer Simpson</td>
-</tr><tr><td class="cell c0 lastcol">Task deadline</td>
-    <td class="cell c0 lastcol">18.10.2020, 18:20</td>
-</tr><tr><td class="cell c0 lastcol">Task status</td>
-    <td class="cell c0 lastcol">Done</td>
--->
-
-    <!--
-    <table >
-        <tr>
-            <td>Task id :  3323</td>
-        </tr>
-        <tr>
-            <td>task title : create a project in PHP</td>
-        </tr>
-        <tr>
-            <td>task description:  create a project using arrays of loops and functions</td>
-        </tr>
-        <tr>
-            <td>task owner: Homer Simpson</td>
-        </tr>
-        <tr>
-            <td>task deadline: 18.10.2020, 18:20</td>
-        </tr>
-        <tr>
-            <td>task status: done</td>
-        </tr>
-
-    </table>
-    -->
-
+<style>
+    table, th, td {
+        border: 1px solid #000000;
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 10px;
+    }
+</style>
