358eb0838ae606fa62fc8f9f29e60ca2086625f3
vitaliy-ph <vitaliy255555@gmail.com>
2020-10-19 17:07:12 +0300

    home work 4 (functions and recursion)

--- samples/functions.php
+++ samples/functions.php
@@ -1,20 +1,198 @@
 <?php
 
-declare(strict_types =1);
-error_reporting(E_ALL);
 
 
-
-/**
- * @param string $name
- * @param string $result
- */
-
-function printName(string $name, string $result = 'PHP')
+/*$global ='123';
+function globalFunction()
 {
-    $function = __FUNCTION__;
-    echo "{$name} + {$function} = {$result}", PHP_EOL;
+
+    $function = 111;
+    var_dump('inside>>>' . $GLOBALS,['$global']);    не использовать
+
+
+    $GLOBALS['global'] = mt_rand();
 
 }
 
-//printName(123);
+globalFunction();*/
+
+//var_dump($global, $functionVar);
+
+///////важная
+/*$var =123;
+function globalFunction($var)
+{
+    var_dump('inside >>>' . $var);
+    $var = mt_rand();
+}
+
+globalFunction($var);
+
+var_dump('outside>>>' . $var);*/
+
+
+/*$global = 334;
+function globalFunction($global){
+
+}
+
+$globalFunction2 = static function () use ($global) {
+    var_dump('inside2 >>>' . $global);                  //важное
+    $global = mt_rand ();
+    unset($global);
+
+
+};
+
+$globalFunction2();
+var_dump('outside >>>' . $global);
+$globalFunction2();
+var_dump('outside >>>' . $global);*/
+
+
+
+
+//$globalFunction2();
+//var_dump('outside 2 >>>' . $global);
+
+function linkedFunc(&$link)
+{
+    var_dump('inside >>>' . $link);
+    $link = mt_rand();
+    unset($link);
+}
+
+/*linkedFunc($global);
+var_dump('outside >>>' . $global);*/
+
+/*function returnFunction ($arg)
+{
+    var_dump('inside >>> '. $arg );
+    $arg = mt_rand();
+    return $arg;
+}
+
+$global = returnFunction($global);
+
+var_dump('outside >>> ' . $global);*/
+
+
+/*function returnFunction (string $arg) : string
+{
+    var_dump('inside >>> '. $arg );
+    $arg = (string)mt_rand();
+
+    return $arg;
+
+}
+
+$global = returnFunction($global);
+
+var_dump('outside >>> ' . $global);*/
+
+
+
+
+/*function sum(int $clientId, int...$numbers): int
+{
+    var_dump($clientId);
+    return array_sum($numbers);
+}
+$sum = sum(102, 3, 3, 5, 11);
+var_dump($sum);*/
+
+//list ($t1, $t2) =[1111, 2222];
+//var_dump($t1,$t2);
+
+/////////home work////////////
+//print r and recursion print r
+
+$Hm = [
+    'country' => 'Ukraine',
+    'city' => 'Lviv',
+    'date' => '19 september',
+    'time' => '14:29'
+];
+
+print_r($Hm);
+
+
+// вариант 1
+
+function showWay($ways = [1]) {
+
+    $Hm = [
+        'country' => 'Ukraine',
+        'city' => 'Lviv',
+        'date' => '19 september',
+        'time' => '14:29'
+    ];
+
+
+$end = end($ways);
+$ways[] = $Hm[$end];
+
+return $Hm[$end] === null ? $ways : showWay($ways);
+}
+
+print_r($Hm);
+
+//print_r(array_count_values($Hm));
+
+
+
+//echo count($Hm);
+
+/*/// вариант 2
+
+$Hm = [
+    'country' => 'Ukraine',
+    'city' => 'Lviv',
+    'date' => '19 september',
+    'time' => '14:29'
+];
+
+
+function show_arr($Hm,$i='')
+{
+    foreach($Hm as $key=>$value)
+    {
+        echo $i."[".$key."] => ";
+
+        if (is_array($value))
+        {
+
+            echo "Array\n";
+            show_arr($value,$i.="\t");
+        }
+        else
+        {echo $value."\n";}
+
+    }
+}
+show_arr($Hm);
+
+//// count*/
+
+$Hm = [
+    'Ukraine'=> [
+        'city' => 'Lviv',
+        'date' => '19 september',
+        'time' => '14:29'
+        ],
+    'Russia' => [
+        'city' => 'Moscow',
+        'date' => '24 september',
+        'time' => '17:11'
+
+    ],
+];
+print_r($Hm);
+
+echo "Count all elements: " . count($Hm,COUNT_RECURSIVE);
+
+
+
+
+
+
---
+++ samples/recursions.php
@@ -0,0 +1,111 @@
+<?php
+// вникнуть
+
+
+$pow = 3 ** 5 ;
+var_dump($pow);
+
+function power(int $number, int $power)
+{
+    if ($power === 0 || $number === 1 ) {
+        return 1;
+    }
+
+        if ($power === 1 || $number === 0) {
+            return  $number;
+    }
+    return $number * power($number, $power - 1);
+}
+
+$powRecursive = power (3,5);
+var_dump($powRecursive);
+
+
+// 1 1 2 3 5 8 13 21 34/// фибаначи
+
+
+
+
+/*function fibonacci(int $n)
+{
+        static $count = 0;
+        $count ++;
+
+        if ($n === 0) {
+
+            return 0;
+        }
+        if ($n === 1) {
+
+            return 1;
+        }
+
+        var_dump($count);
+
+    return fibonacci($n - 2) + fibonacci(($n - 1));
+
+}
+
+$fibonacci = fibonacci(13);
+var_dump($fibonacci);*/
+
+
+
+
+$count = 0;                   //важное
+
+function fibonacci(int $n)
+{
+    static $lib = [];
+    global $count;
+    $count ++;
+
+    if ($n === 0 ) {
+        return 0;
+    }
+    if ($n === 1 ) {
+        return 1;
+    }
+    if (array_key_exists($n,$lib)) {
+        return $lib[$n];
+    }
+
+    $index1 = $n -2;
+    $number1 = fibonacci($index1);
+
+    $index2 = $n -1;
+    $number2 = fibonacci($index2);
+
+        $lib[$index1] = $number1;
+        $lib[$index2] = $number2;
+
+    return $number1 + $number1;
+}
+
+$fibonacci = fibonacci(10);
+var_dump($fibonacci, $count);
+
+
+/*$count = 0;
+
+function fibonacci(int $n)
+{
+    global $count;
+    $count ++;
+
+    if ($n === 0) {
+
+        return 0;
+    }
+    if ($n === 1) {
+
+        return 1;
+    }
+
+
+
+    return fibonacci($n - 2) + fibonacci(($n - 1));
+
+}
+$fibonacci = fibonacci(10);
+var_dump($fibonacci, $count);*/
