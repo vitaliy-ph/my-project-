4c10c7bdd54c2e7a9d5d63d8e22eb172630bf528
vitaliy-ph <vitaliy255555@gmail.com>
2020-10-26 15:08:20 +0300

    home work

--- homework/2020.10.12/index.php
+++ homework/2020.10.12/index.php
@@ -1,62 +1,59 @@
 <?php
 
-/// FOREACH
 
-/*$info = [
-
-    'task id' => 666,
-    'task title' => 'create arrays',
-    'task description' => 'create an array with groups for the next session',
-    'task owner' => [
-        'name one' => 'Homer Simpson',
-        'name two' => 'Bart Simpson',
-    ] ,
-    'task deadline' => '18.10.2020, 18:20',
-    'task status' => [
-        'Homer Simpson' => 'done',
-        'Bart Simpson' => 'in process',
-    ],
-];
-
-
-foreach($info as $key => $value) {
-    echo "{$key}: {$value}", PHP_EOL;
-}*/
-
-
-
-
-
-/////////таблица
-
+error_reporting(E_ALL);
 
 if (1 > 0) {
-echo '<h1>Task manager</h1>';
+    echo '<h1>Task manager</h1>';
+
+
 }
 $info = [
+    [
+        'task id' => 1,
+        'task title' => 'create arrays',
+        'task description' => 'create an array with groups for the next session',
+        'task owner' => 'Homer Simpson',
+        'task deadline' => '18.10.2020, 18:20',
+        'task status' => ' done ',
 
-'task id' => 2,
-'task title' => 'create arrays',
-'task description' => 'create an array with groups for the next session',
-'task owner: number one' => 'Homer Simpson',
-'task owner: number two' =>' Bart Simpson',
-'task deadline' => '18.10.2020, 18:20',
-'task status: number one' => ' done  ',
-'task status: number two' => ' in process ',
+    ],
+    [
+        'task id' => 2,
+        'task title' => 'output an array with the function',
+        'task description' => 'output an array using the for and foreach functions',
+        'task owner' => 'Bart Simpson',
+        'task deadline' => '22.10.2020, 12:54',
+        'task status' => ' in process',
+    ],
 
 ];
 
 ?>
-<table class="Task manager" style="width:50%"
 
-<?php
-foreach($info as $key => $value) {
-    echo '<tr>';
-    echo '<td>' . $key . '</td>';
-    echo '<td>' . ($value) . '</td>';
-    echo '<tr>';
-}
-?>
+
+<table>
+    <tr>
+        <th>task id</th>
+        <th>task title</th>
+        <th>task owner</th>
+        <th>task description</th>
+        <th>task deadline</th>
+        <th>task status</th>
+    </tr>
+
+    <?php
+    foreach ($info as $element) {
+        echo "<tr>
+            <td>{$element['task id']}</td>
+            <td>{$element['task title']}</td>
+            <td>{$element['task owner']}</td>
+            <td>{$element['task description']}</td>
+            <td>{$element['task deadline']}</td>
+            <td>{$element['task status']}</td>
+            </tr>";
+    }
+    ?>
 </table>
 
 
@@ -70,3 +67,7 @@ foreach($info as $key => $value) {
         padding: 10px;
     }
 </style>
+
+
+
+
