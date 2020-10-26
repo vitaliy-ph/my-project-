2adc01f033fcfa0d59945ad56efd6765d873f3bf
vitaliy-ph <vitaliy255555@gmail.com>
2020-10-26 15:10:59 +0300

    home work

--- homework/2020.10.07/index.php
+++ homework/2020.10.07/index.php
@@ -3,19 +3,24 @@
 
 
 $info = [
+    [
+        'task id' => 1,
+        'task title' => 'create arrays',
+        'task description' => 'create an array with groups for the next session',
+        'task owner' => 'Homer Simpson',
+        'task deadline' => '18.10.2020, 18:20',
+        'task status' => ' done ',
 
-    'task id' => 2,
-    'task title' => 'create arrays',
-    'task description' => 'create an array with groups for the next session',
-    'task owner' => [
-        'name 1' => 'Homer Simpson',
-        'name 2' => 'Bart Simpson',
-    ] ,
-    'task deadline' => '18.10.2020, 18:20',
-    'task status' => [
-        'Homer Simpson' => 'done',
-        'Bart Simpson' => 'in process',
     ],
+    [
+        'task id' => 2,
+        'task title' => 'output an array with the function',
+        'task description' => 'output an array using the for and foreach functions',
+        'task owner' => 'Bart Simpson',
+        'task deadline' => '22.10.2020, 12:54',
+        'task status' => ' in process',
+    ],
+
 ];
 
 var_dump($info);
