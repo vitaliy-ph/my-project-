789520f2d88adcddc9094dbc1fbf26a3da07f7e3
vitaliy-ph <vitaliy255555@gmail.com>
2020-11-20 18:55:42 +0300

    homework

--- homework/2020.10.12/index.php
+++ homework/2020.10.12/index.php
@@ -1,4 +1,4 @@
-<h4>FOREACH</h4>
+<h4>FOREACH and FOR</h4>
 <?php
 
 error_reporting(E_ALL);
@@ -11,7 +11,7 @@ $info = [
         'task description' => 'create an array with groups for the next session',
         'task owner' => 'Homer Simpson',
         'task deadline' => '18.10.2020, 18:20',
-        'task status' => ' done ',
+        'task status' => 'done',
 
     ],
     [
@@ -20,35 +20,24 @@ $info = [
         'task description' => 'output an array using the for and foreach functions',
         'task owner' => 'Bart Simpson',
         'task deadline' => '22.10.2020, 12:54',
-        'task status' => ' in process',
+        'task status' => 'in process',
     ],
 
 ];
 
 
+
+
 echo '<table border="1px">';
-foreach ($info as $task => $element) {
-    foreach ($element as $key => $value)
-        echo "<tr><th>$key</th> <td>$value</td></tr>";
+foreach ($info as $task ) {
+    foreach ($task as $key => $value)
+        echo "<tr><th>$key</th>  <td>$value</td></tr>";
 }
-echo "</table>"
+
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Homework</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width">
-</head>
-<body>
-<h4>FOR</h4>
+
+
 <table>
     <tr>
         <th>task id</th>
@@ -58,8 +47,8 @@ echo "</table>"
         <th>task deadline</th>
         <th>task status</th>
     </tr>
-    <?php
-    for ($i = 0; $i < 2; $i++) {
+<?php
+   for ($i = 0; $i < 2; $i++) {
         echo "<tr>
             <td>{$info[$i]['task id']}</td>
             <td>{$info[$i]['task title']}</td>
@@ -69,7 +58,7 @@ echo "</table>"
             <td>{$info[$i]['task status']}</td>
             </tr>";
     }
-    ?>
+?>
 </table>
 </body>
 </html>
@@ -81,6 +70,13 @@ echo "</table>"
         border-collapse: collapse;
     }
     th, td {
-        padding: 10px;
+        padding: 5px;
     }
 </style>
+
+
+
+
+
+
+
