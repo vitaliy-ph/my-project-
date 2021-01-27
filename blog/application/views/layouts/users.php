47a62d0ef9e78f640e8b9811aa04c435e542decc
vitaliy-ph <vitaliy255555@gmail.com>
2021-01-27 12:50:36 +0300

    My project blog and register

---
+++ blog/application/views/layouts/users.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="/public/styles/bootstrap.css" rel="stylesheet">
+    <link href="/public/styles/main.css" rel="stylesheet">
+    <link href="/public/styles/font-awesome.css" rel="stylesheet">
+    <script src="/public/scripts/jquery.js"></script>
+    <script src="/public/scripts/form.js"></script>
+    <script src="/public/scripts/popper.js"></script>
+    <script src="/public/scripts/bootstrap.js"></script>
+</head>
+<body>
+<?php echo $content; ?>
+<hr>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <p class="copyright text-muted">&copy; 2020-2021, PHP</p>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
