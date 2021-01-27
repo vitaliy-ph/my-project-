c6e1b9c0dc4c741991a227a3d5ec1e5f92c53e06
vitaliy-ph <vitaliy255555@gmail.com>
2021-01-27 16:44:34 +0300

    My project blog and register

--- blog/application/views/admin/edit.php
+++ blog/application/views/admin/edit.php
@@ -1,7 +1,7 @@
 <div class="content-wrapper">
     <div class="container-fluid">
         <div class="card mb-3">
-            <div class="card-header">Edit post</div>
+            <div class="card-header"><?php echo $title; ?></div>
             <div class="card-body">
                 <div class="row">
                     <div class="col-sm-5">
--- blog/application/views/layouts/default.php
+++ blog/application/views/layouts/default.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title><?php echo $title; ?></title>
     <link href="/public/styles/bootstrap.css" rel="stylesheet">
     <link href="/public/styles/main.css" rel="stylesheet">
     <link href="/public/styles/font-awesome.css" rel="stylesheet">
